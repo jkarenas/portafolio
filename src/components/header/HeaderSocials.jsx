d44badf4e614d440e6bede72ea286dc0e2511498
jkarenas <jc.arenas1993@gmail.com>
2023-10-25 19:12:40 -0500

    deploy

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,9 +7,9 @@ import {BsDribbble} from 'react-icons/bs'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href='https://linkedin.com' target="_blank"><BsLinkedin/></a>
-        <a href='https://github.com' target="_blank"><FaGithub/></a>
-        <a href='https://dribble.com' target="_blank"><BsDribbble/></a>
+        <a href='https://linkedin.com' target="_blank" rel="noreferrer"><BsLinkedin/></a>
+        <a href='https://github.com' target="_blank" rel="noreferrer"><FaGithub/></a>
+        <a href='https://dribble.com' target="_blank" rel="noreferrer"><BsDribbble/></a>
 
     </div>
   )
